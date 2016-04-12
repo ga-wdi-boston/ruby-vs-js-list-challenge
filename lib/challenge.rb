eab9bd8d0736c7db6135a5cf4bb25fb34f27f7f8
soynog <nicolas.o.garcia@gmail.com>
2016-04-12 09:27:49 -0400

    Ruby vs. JS None? Challenge Response

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -10,6 +10,15 @@ def any? ary, &block
   return false
 end
 
+def none? ary, &block
+  ary.each do |i|
+    if block.call(i)
+      return false
+    end
+  end
+  return true
+end
+
 ary1 = [1,3,5,7]
 ary2 = [1,2,3,4]
 
@@ -30,6 +39,26 @@ end)
 
 # Should print false
 puts (any? ary2 do |i|
+  i > 6
+end)
+
+# Should print true
+puts (none? ary1 do |i|
+  i % 2 == 0
+end)
+
+# Should print false
+puts (none? ary2 do |i|
+  i % 2 == 0
+end)
+
+# Should print false
+puts (none? ary1 do |i|
+  i > 6
+end)
+
+# Should print true
+puts (none? ary2 do |i|
   i > 6
 end)
 
