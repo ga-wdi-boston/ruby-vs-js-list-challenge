812bcf32828098b7613dd75a4c1145d05783f7c8
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-14 09:27:24 -0400

    Write none? method in RB
    Write none? method
    Takes a list and a block
    Returns true if block returns false for all list items
    Otherwise, returns false

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -6,3 +6,12 @@ def any? list, &block
   end
   return false;
 end
+
+def none? list, &block
+  list.each do |item|
+    if block.call(item)
+      return false;
+    end
+  end
+  return true;
+end
