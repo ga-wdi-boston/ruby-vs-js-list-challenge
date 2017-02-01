874d448ffb3577be18c55aa10979d5ad639fd09a
novakw267 <novakw267@gmail.com>
2017-02-01 09:21:56 -0500

    I took my best guess.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,23 +1,23 @@
 # Requirements
 
 # Add your function parameters between the parentheses.
-def any?()
-  # Your function body here.
+def any?(array)
+  # array.any?(&even?)
 end
 
 # Add your function parameters between the parentheses.
-def none?()
-  # Your function body here.
+def none?(array)
+  # array.none?(&even?)
 end
 
 # Bonuses
 
 # Add your function parameters between the parentheses.
-def one?()
-  # Your function body here.
+def one?(array)
+  # array.one?(&odd?)
 end
 
 # Add your function parameters between the parentheses.
-def all?()
-  # Your function body here.
+def all?(array)
+  # array.all?(&odd?)
 end
