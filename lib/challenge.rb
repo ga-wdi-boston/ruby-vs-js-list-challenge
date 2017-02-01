a5fe2d932afa76a2bd312b2492a2123864996f4c
akgoode <akgoode@gmail.com>
2017-02-01 10:02:10 -0500

    Add answers to ruby challenge
    Sam helped me with how to use yield

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,23 +1,50 @@
 # Requirements
 
 # Add your function parameters between the parentheses.
-def any?(*list, &block)
-  if list.find(block)
-    true
+def any?(array)
+  if array.length.zero?
+    return false
   end
-  false
+
+  0.upto(array.length - 1) do |i|
+    if yield array[i]
+      return true
+    end
+  end
+  return false
 end
 
 # Add your function parameters between the parentheses.
-def none?()
-  # Your function body here.
+def none?(array)
+  if array.length.zero?
+    return true
+  end
+  0.upto(array.length - 1) do |i|
+    if yield array[i]
+      return false
+    end
+  end
+  true
 end
 
 # Bonuses
 
 # Add your function parameters between the parentheses.
-def one?()
-  # Your function body here.
+def one?(array)
+  if array.length.zero?
+    return false
+  end
+  count = 0
+  0.upto(array.length) do |i|
+    if yield array[i]
+      count += 1
+    end
+  end
+  if count > 1 || count < 1
+    return false
+  else
+    return true
+  end
 end
 
 # Add your function parameters between the parentheses.
