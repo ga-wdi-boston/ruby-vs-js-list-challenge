342d10a407971400e00908163c47729a485c285d
akgoode <akgoode@gmail.com>
2017-02-01 14:16:32 -0500

    add answers to ruby challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,53 +1,24 @@
 # Requirements
 
 # Add your function parameters between the parentheses.
-def any?(array)
-  if array.length.zero?
-    return false
-  end
 
-  0.upto(array.length - 1) do |i|
-    if yield array[i]
-      return true
-    end
-  end
-  return false
+def any?(array, &block)
+  array.any?(&block)
 end
 
 # Add your function parameters between the parentheses.
-def none?(array)
-  if array.length.zero?
-    return true
-  end
-  0.upto(array.length - 1) do |i|
-    if yield array[i]
-      return false
-    end
-  end
-  true
+def none?(array, &block)
+  array.none?(&block)
 end
 
 # Bonuses
 
 # Add your function parameters between the parentheses.
-def one?(array)
-  if array.length.zero?
-    return false
-  end
-  count = 0
-  0.upto(array.length) do |i|
-    if yield array[i]
-      count += 1
-    end
-  end
-  if count > 1 || count < 1
-    return false
-  else
-    return true
-  end
+def one?(array, &block)
+  array.one?(&block)
 end
 
 # Add your function parameters between the parentheses.
-def all?()
-  # Your function body here.
+def all?(array, &block)
+  array.all?(&block)
 end
